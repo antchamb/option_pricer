b96c0dd8b021603b8c5b795af79029a0df678875
Antoine Chambellan <antoine.chambellan2@gmail.com>
2024-10-14 17:21:21 +0200

    pages structure

--- pages/Merton.py
+++ pages/Merton.py
@@ -1,7 +1,7 @@
 import dash
 from dash import html, dcc, Input, Output, State, callback
 
-dash.register_page(__name__, name='Binom', path='/binom')
+dash.register_page(__name__, name='Merton', path='/merton')
 
 layout = html.Div([])
 
